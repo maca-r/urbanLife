0f6983a538791b6c47d6c9a3c1e26df7706bb170
maca-r <macarenagreyes@gmail.com>
2023-08-05 11:45:36 -0300

    bootstrap instalado

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,27 +5,6 @@ const Home = () => {
     return (
         <div className={styles.body}>
 
-            <div className={styles.categorias}>
-                <form action="">
-                    <select name="" id="">
-                        <option selected>Categorias</option>
-                        <option value="uno">uno</option>
-                        <option value="dos">dos</option>
-                        <option value="tres">tres</option>
-                    </select>
-                    
-                </form>
-                <img src="" alt="" />
-            </div>
-        
-            <div className={styles.buscador}>
-                <p>Buscar...</p>
-            </div>
-
-            <div className={styles.imagenCategoria}>
-                <img src="" alt="" />
-            </div>
-
             
         </div>
     )
