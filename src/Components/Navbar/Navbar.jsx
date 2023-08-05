622e4c25980432e12a9dd019541bf990cfcd94da
maca-r <macarenagreyes@gmail.com>
2023-08-05 00:58:42 -0300

    modifica css navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,11 +6,12 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
     return (
         <nav>
+        
             <Link to={routes.home} className={styles.logo}>
                 <img src="./images/logo.png" alt="logo Urban Life"/>
                 <h6>lema</h6>
             </Link>
-
+            
             <div className={styles.rutas}>
                 <Link to={routes.signUp} className={styles.crearCuenta}>Crear cuenta</Link>
                 <Link to={routes.logIn} className={styles.iniciarSesion}>Iniciar sesión</Link>
--- src/Components/ProductDetail/Detail.jsx
+++ src/Components/ProductDetail/Detail.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Detail.module.css'
 
 const Detail = () => {
   return (
