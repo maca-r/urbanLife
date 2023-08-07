56d5a309cc82ab29928f3b7aa4631a6d7850c63b
maca-r <macarenagreyes@gmail.com>
2023-08-07 20:13:46 -0300

    modificaciones home y detail desktop

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.css'
 
 const Home = () => {
+    const [searchText, setSearchText] = useState('');
+
+    const handleChange = (e) => {
+        setSearchText(e.target.value);
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // realizariamos las solicitudes a la API.
+        console.log('Texto de búsqueda:', searchText);
+    };  
     return (
         <div className={styles.body}>
 
-        
+        {/* BUSCADOR */}
+
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Buscar..."
+                value={searchText}
+                onChange={handleChange}
+            />
+            {/* <button type="submit">Buscar</button> */}
+        </form>
+
+        {/* CATEGORIA */}
+
+        <h2>CATEGORIAS</h2>
+        <div className={styles.categoria}>
+            <div>
+                <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                <h4>Categoria 1</h4>
+                <p>$$$$</p>
+            </div>
+            <div>
+                <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                <h4>Categoria 2</h4>
+                <p>$$$$</p>
+            </div>
+            <div>
+                <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                <h4>Categoria 3</h4>
+                <p>$$$$</p>
+            </div>
+            <div>
+                <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                <h4>Categoria 4</h4>
+                <p>$$$$</p>
+            </div>
         </div>
+
+        {/* PRODUCTOS ALEATORIOS */}
+        <h2>PRODUCTOS ALEATORIOS</h2>
+        <div  className={styles.producAleatorio}>
+            <div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://vcp.com.ar/cdn/shop/products/VCP7marzo_22-191.jpg?v=1646746906" alt="" />
+                    <div>
+                        <p>Product Name</p>
+                        <p>Lorem ipsum dolor sit amet   consectetur adipisicing elit.     Aspernatur delectus quasi recusandae</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    
+    </div>
+        
     )
 }
 
--- src/Components/ProductDetail/Detail.jsx
+++ src/Components/ProductDetail/Detail.jsx
@@ -3,6 +3,10 @@ import { DatePicker } from '@mui/x-date-pickers'
 import styles from './Detail.module.css'
 
 import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 
 
@@ -31,7 +35,21 @@ const Detail = () => {
 
   },[dimensions])
 
-  {console.log(carouselVisible,dimensions)}
+  
+
+  const images = [
+    'https://images.unsplash.com/photo-1602810316498-ab67cf68c8e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1603252109612-24fa03d145c8?ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1603251578711-3290ca1a0187?ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+  ];
+
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const handleImageClick = (index) => {
+    setSelectedImage(images[index]);
+  };
+
+
   return (
     <div className={styles.detalleProducto}>
             
@@ -54,25 +72,45 @@ const Detail = () => {
             style={{width:"100%"}}/>
         </Carousel.Item>
       </Carousel>
+      
+      <div className={styles.containerImg}>
+        
+          <div className={styles.largeImg}>
+            <Image src={selectedImage} fluid/>
+          </div>
+          <div className={styles.smallImg}>
+            {images.map((image, index) => (
+              <Image
+                key={index}
+                src={image}
+                className={`${selectedImage === image ? 'selected' : ''}`}
+                onClick={() => handleImageClick(index)}
+                thumbnail
+                style={{maxWidth: "auto",
+                  height: "auto",
+                  cursor: "pointer",
+                  }}
+              />
+            ))}
+          </div>
+      
+    </div>
 
-      <div className={styles.images}>
-        
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-      </div>
+    <button></button>
       
-      
-      <div className={styles.precioReserva} >
-        <h5>PRECIO: $100</h5>
-        <button>RESERVAR</button>
-      </div>
-        
-      <div className={styles.tallesReserva}>
+
+    <div className={styles.tallesReserva}>
         <button>TALLE S</button>
         <button>TALLE M</button>
         {/* <DatePicker className={styles.datePicker} /> */}
-      </div>
+    </div>
+
+    <div className={styles.precioReserva} >
+        <h5>PRECIO: $100</h5>
+        <button>RESERVAR</button>
+    </div>
+        
+      
 
     </div>
   )
