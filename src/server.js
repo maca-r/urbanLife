d4e3b467d55dfa8afff408fe9fe710de5987caa1
Luca Carena <lucacarena98@gmail.com>
2023-08-14 11:37:08 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
-// Add CORS headers middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your frontend's URL
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-
-// Rest of your server code
 
 app.listen(80, () => {
   console.log("Server is running on port 80");
