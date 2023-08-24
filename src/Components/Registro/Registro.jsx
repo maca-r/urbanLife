a2e6b3f88952a977649cfeae13705a9b6cc71c6b
Luca Carena <lucacarena98@gmail.com>
2023-08-24 19:23:33 -0300

    gmail

--- src/Components/Registro/Registro.jsx
+++ src/Components/Registro/Registro.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import {  useNavigate } from "react-router-dom";
-import {useEffect} from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 import styles from "./Registro.module.css";
 
 const Registro = () => {
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [gmail, setGmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [nombreError, setNombreError] = useState('');
-  const [apellidoError, setApellidoError] = useState('');
-  const [gmailError, setGmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [gmail, setGmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nombreError, setNombreError] = useState("");
+  const [apellidoError, setApellidoError] = useState("");
+  const [gmailError, setGmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleNombreChange = (event) => {
     const inputValue = event.target.value;
@@ -20,10 +20,12 @@ const Registro = () => {
       const formattedValue =
         inputValue.charAt(0).toUpperCase() + inputValue.slice(1).toLowerCase();
       setNombre(formattedValue);
-      setNombreError('');
+      setNombreError("");
     } else {
-      setNombre('');
-      setNombreError('El nombre debe contener solo letras y la primera letra en mayúscula.');
+      setNombre("");
+      setNombreError(
+        "El nombre debe contener solo letras y la primera letra en mayúscula."
+      );
     }
   };
 
@@ -33,10 +35,12 @@ const Registro = () => {
       const formattedValue =
         inputValue.charAt(0).toUpperCase() + inputValue.slice(1).toLowerCase();
       setApellido(formattedValue);
-      setApellidoError('');
+      setApellidoError("");
     } else {
-      setApellido('');
-      setApellidoError('El apellido debe contener solo letras y la primera letra en mayúscula.');
+      setApellido("");
+      setApellidoError(
+        "El apellido debe contener solo letras y la primera letra en mayúscula."
+      );
     }
     // if (apellido.length < 2){
     //   setApellidoError('El apellido debe tener por lo menos 3 letras')
@@ -46,109 +50,128 @@ const Registro = () => {
   };
 
   const handleGmailChange = (event) => {
-    
-    
     setGmail(event.target.value);
-    
   };
 
   const handlePasswordChange = (event) => {
     const newPassword = event.target.value;
     setPassword(newPassword);
     if (newPassword.length < 5) {
-      setPasswordError('La contraseña debe tener al menos 5 caracteres.');
+      setPasswordError("La contraseña debe tener al menos 5 caracteres.");
     } else {
-      setPasswordError('');
+      setPasswordError("");
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = () => {
     // event.preventDefault();
 
-    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+=?`{|}~^.-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/);
+    const emailRegex = new RegExp(
+      /^[A-Za-z0-9_!#$%&'*+=?`{|}~^.-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+    );
 
-    if(emailRegex.test(gmail)){
-      setGmail(gmail)
-      console.log('Nombre:', nombre);
-      console.log('Apellido:', apellido);
-      console.log('Correo electrónico:', gmail);
-      console.log('Contraseña:', password);
+    if (emailRegex.test(gmail)) {
+      setGmail(gmail);
+      console.log("Nombre:", nombre);
+      console.log("Apellido:", apellido);
+      console.log("Correo electrónico:", gmail);
+      console.log("Contraseña:", password);
 
       const userData = { nombre, apellido, gmail, password };
-      localStorage.setItem('userData', JSON.stringify(userData));
-      
-      setNombre('');
-      setApellido('');
-      setGmail('');
-      setPassword('');
+      localStorage.setItem("userData", JSON.stringify(userData));
+
+      setNombre("");
+      setApellido("");
+      setGmail("");
+      setPassword("");
+
+      handleSubmitInicioSesion();
     } else {
-      
-      setGmail('')
-      setGmailError('El mail no es válido')
+      setGmail("");
+      setGmailError("El mail no es válido");
     }
 
-    
-    //REDIRIJO 
-    handleSubmitInicioSesion()
+    //REDIRIJO
+    // handleSubmitInicioSesion();
+  };
 
-    };
+  // REDIRIJO A HOME (CON LOS CAPOS CORRECTOS)
 
+  const navigate = useNavigate();
 
-    // REDIRIJO A HOME (CON LOS CAPOS CORRECTOS)
-
-
-    const navigate = useNavigate()
-
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('userData'));
+    const items = JSON.parse(localStorage.getItem("userData"));
     if (items) {
       setItems(items);
     }
-}, []);
+  }, []);
 
-    const handleSubmitInicioSesion = () => {
-      
-      console.log(items);
-      if (items !== []) {
-          return(navigate("/"))
-      }else{
-        console.log(false);
-      }
-  }
-    
-    
-    
-    
+  const handleSubmitInicioSesion = () => {
+    console.log(items);
+    if (items !== []) {
+      return navigate("/");
+    } else {
+      console.log(false);
+    }
+  };
 
   return (
     <form className={styles.formLogin} onSubmit={handleSubmit}>
       <div className={styles.div}>
         <label>Nombre:</label>
-        <input type="text" value={nombre} onChange={handleNombreChange} required/>
+        <input
+          type="text"
+          value={nombre}
+          onChange={handleNombreChange}
+          required
+        />
         {nombreError && <p className={styles.errorMessage}>{nombreError}</p>}
       </div>
       <div className={styles.div}>
         <label>Apellido:</label>
-        <input type="text" value={apellido} onChange={handleApellidoChange} required/>
-        {apellidoError && <p className={styles.errorMessage}>{apellidoError}</p>}
+        <input
+          type="text"
+          value={apellido}
+          onChange={handleApellidoChange}
+          required
+        />
+        {apellidoError && (
+          <p className={styles.errorMessage}>{apellidoError}</p>
+        )}
       </div>
       <div className={styles.div}>
         <label>Correo electrónico:</label>
-        <input type="email" value={gmail} onChange={handleGmailChange} required/>
+        <input
+          type="email"
+          value={gmail}
+          onChange={handleGmailChange}
+          required
+        />
         {gmailError && <p className={styles.errorMessage}>{gmailError}</p>}
       </div>
       <div className={styles.div}>
         <label>Contraseña:</label>
-        <input type="password" value={password} onChange={handlePasswordChange} required/>
-        {passwordError && <p className={styles.errorMessage}>{passwordError}</p>}
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        {passwordError && (
+          <p className={styles.errorMessage}>{passwordError}</p>
+        )}
       </div>
-      <button className={styles.boton} type="submit"
-      onClick={()=>{
-        handleSubmit()
-      }}
-      >Registrarse</button>
+      <button
+        className={styles.boton}
+        type="submit"
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
+        Registrarse
+      </button>
     </form>
   );
 };
