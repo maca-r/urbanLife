965adfa81370381301307ba707b654e7e264fc72
Luca Carena <lucacarena98@gmail.com>
2023-08-24 19:50:21 -0300

    logout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,46 +1,54 @@
 import { Link } from "react-router-dom";
 import { routes } from "../../Routes/routes";
 import styles from "./Navbar.module.css";
-import {  useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 const Navbar = () => {
-
-
   const [user, setUser] = useState([]);
   const [userData, setUserData] = useState([]);
   const [inicialesUserData, setInicialesUserData] = useState("");
-  const [inicialesUSER, setInicialesUSER] = useState('')  
+  const [inicialesUSER, setInicialesUSER] = useState("");
 
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    const user = JSON.parse(localStorage.getItem('user'));
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (userData) {
       setUserData(userData);
       const initials = userData.nombre[0] + userData.apellido[0];
       const upperCaseInitials = initials.toUpperCase();
-      setInicialesUserData(upperCaseInitials); 
+      setInicialesUserData(upperCaseInitials);
     }
     if (user) {
-      setUser(user)
-      const initialsUser = user.email[0] + user.email[1]
+      setUser(user);
+      const initialsUser = user.email[0] + user.email[1];
       const upperCaseInitialsUser = initialsUser.toUpperCase();
-      setInicialesUSER(upperCaseInitialsUser)
-
+      setInicialesUSER(upperCaseInitialsUser);
     }
   }, []);
-  
-
 
   const logout = () => {
-    //localStorage.removeItem('userData');
-    localStorage.removeItem('user');
-    setUserData([])
-    setUser([])
-  }
+    setShowLogoutModal(true); // Mostrar el modal al hacer clic en "Cerrar sesión"
+  };
 
+  const confirmLogout = () => {
+    localStorage.removeItem("user");
+    setUserData([]);
+    setUser([]);
+    setShowLogoutModal(false); // Cierra el modal después de confirmar el cierre de sesión
+  };
 
+  // const logout = () => {
+  //   //localStorage.removeItem('userData');
+  //   const confirmLogout = window.confirm("¿Seguro que quieres cerrar sesión?");
+  //   if (confirmLogout) {
+  //     localStorage.removeItem("user");
+  //     setUserData([]);
+  //     setUser([]);
+  //   }
+  // };
 
   const renderProfileImageOrLoading = () => {
     if (userData.nombre !== "" && inicialesUserData !== "") {
@@ -53,37 +61,52 @@ const Navbar = () => {
   };
 
   return (
-
     <nav>
       <Link to={routes.home} className={styles.logo}>
         <div className={styles.logo}>
-          <img className={styles.logoImage} src="/images/logo.png" alt="logo Urban Life" />
+          <img
+            className={styles.logoImage}
+            src="/images/logo.png"
+            alt="logo Urban Life"
+          />
         </div>
         <h6>Donde el diseño encuentra a todos</h6>
       </Link>
 
       <div className={styles.rutas}>
-
-      {
-          userData.length == 0 || !localStorage.getItem('user')? (
-            <>
-              <Link to={routes.registro} className={styles.crearCuenta}>
-                Crear cuenta
-              </Link>
-              <Link to={routes.login} className={styles.iniciarSesion}>
-                Iniciar sesión
-              </Link>
-            </>
-          ) : (
-            <div className={styles.cerrarSesionBOX}>
-
+        {userData.length == 0 || !localStorage.getItem("user") ? (
+          <>
+            <Link to={routes.registro} className={styles.crearCuenta}>
+              Crear cuenta
+            </Link>
+            <Link to={routes.login} className={styles.iniciarSesion}>
+              Iniciar sesión
+            </Link>
+          </>
+        ) : (
+          <div className={styles.cerrarSesionBOX}>
             {renderProfileImageOrLoading()}
 
-            <button className={styles.logOut} onClick={logout}>Cerrar sesión</button>
-            </div>
-          )
-        }
+            <button className={styles.logOut} onClick={logout}>
+              Cerrar sesión
+            </button>
+          </div>
+        )}
       </div>
+
+      <Modal show={showLogoutModal} onHide={() => setShowLogoutModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>¿Seguro que quieres cerrar sesión?</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowLogoutModal(false)}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={confirmLogout}>
+            Cerrar sesión
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </nav>
   );
 };
