b03ad087b6d9122dc19a9966412c936382a32204
Luca Carena <lucacarena98@gmail.com>
2023-08-29 12:21:27 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-const express = "express";
-const app = express();
+// const express = "express";
+// const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
-app.listen(80, () => {
-  console.log("Servidor funcionando en el puerto 80");
-});
+// app.listen(80, () => {
+//   console.log("Servidor funcionando en el puerto 80");
+// });
