0700b9e601d6402ee5f5e058775958efa3d950e1
JhonatanCremona <jfulguera@creminox.com>
2023-08-31 18:49:17 -0300

    S3Service: semi-completo upload - getObject

--- src/main/java/com/urbanlife/urbanlife/s3/S3Service.java
+++ src/main/java/com/urbanlife/urbanlife/s3/S3Service.java
@@ -2,8 +2,6 @@ package com.urbanlife.urbanlife.s3;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
