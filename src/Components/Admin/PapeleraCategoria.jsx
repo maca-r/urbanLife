35922fe0636b0b61b62e84340f00ba96bfb63ba3
Luca Carena <lucacarena98@gmail.com>
2023-09-04 17:33:20 -0300

    papelera

--- src/Components/Admin/PapeleraCategoria.jsx
+++ src/Components/Admin/PapeleraCategoria.jsx
@@ -1,3 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function PapeleraCategoria() {
-  return <section></section>;
+  const [categoriasEliminadas, setCategoriasEliminadas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:80/categorias/listarcategorias-all")
+      .then((response) => {
+        const categoriasEliminadas = response.data.filter(
+          (categoria) => categoria.eliminarCategoria === true
+        );
+        setCategoriasEliminadas(categoriasEliminadas);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener las categorías eliminadas:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <section>
+      <h3>Categorías Eliminadas</h3>
+      {loading ? (
+        <p>Cargando categorías eliminadas...</p>
+      ) : categoriasEliminadas.length === 0 ? (
+        <p>No hay categorías eliminadas</p>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Título</th>
+              <th>Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categoriasEliminadas.map((categoria) => (
+              <tr key={categoria.idCategoria}>
+                <td>{categoria.idCategoria}</td>
+                <td>{categoria.titulo}</td>
+                <td>{categoria.descripcion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </section>
+  );
 }
