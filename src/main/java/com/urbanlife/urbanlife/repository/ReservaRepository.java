ad6566fc87498aee747f06fa87f55dc28275a1c4
JhonatanCremona <jfulguera@creminox.com>
2023-09-06 23:55:28 -0300

    Deploy

--- src/main/java/com/urbanlife/urbanlife/repository/ReservaRepository.java
+++ src/main/java/com/urbanlife/urbanlife/repository/ReservaRepository.java
@@ -3,9 +3,12 @@ package com.urbanlife.urbanlife.repository;
 import com.urbanlife.urbanlife.models.Reservas;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Transactional
 @Repository
 public interface ReservaRepository extends JpaRepository<Reservas, Integer> {
+
 
 }
