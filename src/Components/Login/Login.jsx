4f438e357471e1d6fece208f118e2593435f667d
Luca Carena <lucacarena98@gmail.com>
2023-09-19 12:41:50 -0300

    login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,90 @@
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// // import { routes } from "../../Routes/routes";
+
+// import styles from "./Login.module.css";
+
+// const Login = () => {
+//   const navigate = useNavigate();
+
+//   const [user, setUser] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const { email, password } = user;
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (user.email === localStorage.getItem("userData").gmail) {
+//       console.log(user.email);
+//       console.log(localStorage.getItem("userData").gmail);
+//     }
+//     console.log(user);
+//     localStorage.getItem("userData").gmail;
+//   };
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     setUser((prevCredentials) => ({
+//       ...prevCredentials,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleSubmitInicioSesion = () => {
+//     if (JSON.stringify(user) === JSON.stringify({ email: "", password: "" })) {
+//       return <p>¡Error! No se encontraron datos de usuario.</p>;
+//     } else {
+//       localStorage.setItem("user", JSON.stringify(user));
+//       navigate("/");
+//       window.location.reload();
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form className={styles.formLogin} onSubmit={handleSubmit}>
+//         <div className={styles.input}>
+//           <label>Correo electrónico:</label>
+//           <input
+//             type="text"
+//             name="email"
+//             value={user.email}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <div className={styles.input}>
+//           <label>Contraseña:</label>
+//           <input
+//             type="password"
+//             name="password"
+//             value={user.password}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <button
+//           className={styles.boton}
+//           type="submit"
+//           onClick={() => {
+//             {
+//               handleSubmitInicioSesion();
+//             }
+//           }}
+//         >
+//           Iniciar sesión
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { routes } from "../../Routes/routes";
 
 import styles from "./Login.module.css";
 
@@ -14,15 +98,23 @@ const Login = () => {
 
   const { email, password } = user;
 
-  const handleSubmit = (e) => {
+  const backendURL = "http://34.229.181.144/api/v1/auth/authenticate";
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (user.email === localStorage.getItem("userData").gmail) {
-      console.log(user.email);
-      console.log(localStorage.getItem("userData").gmail);
+    try {
+      const response = await axios.post(backendURL, user);
+
+      if (response.status === 200) {
+        navigate("/");
+        window.location.reload();
+      } else {
+        console.error("Error de inicio de sesión");
+      }
+    } catch (error) {
+      console.error("Error de red:", error);
     }
-    console.log(user);
-    localStorage.getItem("userData").gmail;
   };
 
   const handleInputChange = (e) => {
@@ -33,48 +125,24 @@ const Login = () => {
     }));
   };
 
-  const handleSubmitInicioSesion = () => {
-    if (JSON.stringify(user) === JSON.stringify({ email: "", password: "" })) {
-      return <p>¡Error! No se encontraron datos de usuario.</p>;
-    } else {
-      localStorage.setItem("user", JSON.stringify(user));
-      navigate("/");
-      window.location.reload();
-    }
-  };
-
   return (
-    <div>
-      <form className={styles.formLogin} onSubmit={handleSubmit}>
-        <div className={styles.input}>
-          <label>Correo electrónico:</label>
-          <input
-            type="text"
-            name="email"
-            value={user.email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className={styles.input}>
-          <label>Contraseña:</label>
-          <input
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button
-          className={styles.boton}
-          type="submit"
-          onClick={() => {
-            {
-              handleSubmitInicioSesion();
-            }
-          }}
-        >
-          Iniciar sesión
-        </button>
+    <div className={styles.loginContainer}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleInputChange}
+          placeholder="Correo electrónico"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+          placeholder="Contraseña"
+        />
+        <button type="submit">Iniciar Sesión</button>
       </form>
     </div>
   );
