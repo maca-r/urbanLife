d0923b9417dcc9a000c217bae86ee29030ef2f17
Luca Carena <lucacarena98@gmail.com>
2023-09-21 16:42:40 -0300

    estilo

--- src/Components/Favs/Favs.jsx
+++ src/Components/Favs/Favs.jsx
@@ -1,65 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Card, Button, ListGroup, Alert } from "react-bootstrap"; // Asegúrate de importar Alert y Button desde react-bootstrap
 import { toast } from "sonner";
-import StarIcon from "@mui/icons-material/Star";
+// import StarIcon from "@mui/icons-material/Star";
 import { useContextoGlobal } from "../GlobalContext";
 
 const Favs = () => {
-  console.log(localStorage.getItem("favs"));
   const [favs, setFavs] = useState([]);
+  const { dataState, dataDispatch } = useContextoGlobal();
 
   useEffect(() => {
     const storedFavs = JSON.parse(localStorage.getItem("favs")) || [];
     setFavs(storedFavs);
   }, []);
 
-  const [favorite, setFavorite] = useState("");
-  const { dataState, dataDispatch } = useContextoGlobal();
-
-  // const addFav = (favorito) => {
-  //     if (!dataState.favs.includes(favorito)) {
-  //         dataDispatch({ type: "LIKE", payload: favorito });
-  //         console.log(favorito);
-  //         setFavorite("like");
-  //         toast.success("Producto agregado a favoritos");
-  //     } else {
-  //         dataDispatch({ type: "DISLIKE", payload: favorito });
-  //         setFavorite("dislike");
-  //     }
-  // };
-
-  const imageUrl = favs.imagenes?.[0]?.urlImagen || "";
+  const removeFav = (favorito) => {
+    // Filtrar la lista de favoritos para quitar el producto seleccionado
+    const updatedFavs = favs.filter(
+      (item) => item.idProducto !== favorito.idProducto
+    );
+    setFavs(updatedFavs);
+    localStorage.setItem("favs", JSON.stringify(updatedFavs));
+    toast.info("Producto eliminado de favoritos");
+  };
 
   return (
     <div>
       <h2>Productos Favoritos</h2>
 
-      {favs.map((favorito, index) => (
-        <ul key={index}>
-          <li
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              margin: "2%",
-            }}
-          >
-            <p>ID del Producto: {favorito.idProducto}</p>
-            <p>Nombre: {favorito.nombre}</p>
-            <p>Precio: $ {favorito.precio}</p>
-            {/* <p>Imágenes: {imageUrl}</p> */}
-            <img style={{ maxWidth: "100px" }} src={imageUrl} alt="" />
-            {/* <button onClick={addFav(favorito)}>
-                    {favorite === "like" ? (
-                        <StarIcon style={{ color: "#E3CE8D" }} />
-                    ) : (
-                        <StarIcon style={{ color: "gray" }} />
-                    )}
-        </button> */}
-          </li>
-        </ul>
-
-        //     // <Card key={index} data={favorito}></Card>
-      ))}
+      {favs.length === 0 ? (
+        <Alert variant="warning">
+          No hay productos favoritos.{" "}
+          <Button variant="primary" href="/">
+            Ir al Home
+          </Button>
+        </Alert>
+      ) : (
+        <ListGroup>
+          {favs.map((favorito, index) => (
+            <ListGroup.Item key={index}>
+              <Card>
+                <Card.Body>
+                  <Card.Text>Nombre: {favorito.nombre}</Card.Text>
+                  <Card.Text>Precio: $ {favorito.precio}</Card.Text>
+                  <Card.Text>Descripcion: {favorito.detalle}</Card.Text>
+                  <img
+                    src={favorito.imagenes?.[0]?.urlImagen || ""}
+                    alt=""
+                    style={{ maxWidth: "100px" }}
+                  />
+                  <Button variant="danger" onClick={() => removeFav(favorito)}>
+                    Quitar de favoritos
+                  </Button>
+                </Card.Body>
+              </Card>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 };
