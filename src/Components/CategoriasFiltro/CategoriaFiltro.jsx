1a1efdb4a1751914904aa8c062a03676cdebd4ab
Luca Carena <lucacarena98@gmail.com>
2023-09-21 16:03:13 -0300

    cat

--- src/Components/CategoriasFiltro/CategoriaFiltro.jsx
+++ src/Components/CategoriasFiltro/CategoriaFiltro.jsx
@@ -1,76 +1,66 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useContextoGlobal } from '../GlobalContext';
-import { useParams } from 'react-router-dom';
-import Card from '../Card/Card';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useContextoGlobal } from "../GlobalContext";
+import { useParams } from "react-router-dom";
+import Card from "../Card/Card";
 import styles from "./CategoriaFiltro.module.css";
 
-
 const CategoriaFiltro = () => {
+  const [categoria, setCategoria] = useState();
 
-    const [categoria,setCategoria] = useState()
+  const { dataState } = useContextoGlobal();
 
-    const { dataState } = useContextoGlobal();
+  const productosCategoria = [];
 
-    const productosCategoria = []
+  const params = useParams();
 
-    const params = useParams()
+  const id = params.id;
 
-    const id = params.id
+  //console.log(`http://localhost/categorias/${id}`);
 
-    //console.log(`http://localhost/categorias/${id}`);
-
-    const publicUrl = import.meta.env.VITE_API_URL_PUBLIC;
-    const privateUrl = import.meta.env.VITE_API_URL_PRIVATE;
-
-  
+  const publicUrl = import.meta.env.VITE_API_URL_PUBLIC;
+  const privateUrl = import.meta.env.VITE_API_URL_PRIVATE;
 
   const urlCategoria =
     privateUrl != ""
       ? `${privateUrl}:80/categorias/categorias/${id}`
       : `${publicUrl}:80/categorias/categorias/${id}`;
 
-    useEffect(() => {
-        try {
-            axios.get(urlCategoria).then((response) => {
-                console.log(response.data);
-                setCategoria(response.data);
-                
-            });
-            } catch (error) {
-                console.error("error al obtener categorias");
-            }
-    },[urlCategoria])
-
-    const filtradoProducto = () => {
-        (dataState.productos).forEach(producto => {
-            
-            if (producto.categorias.idCategoria == id) {
-                //console.log(producto);
-                productosCategoria.push(producto)
-                
-            }
-            
-        });
+  useEffect(() => {
+    try {
+      axios.get(urlCategoria).then((response) => {
+        console.log(response.data);
+        setCategoria(response.data);
+      });
+    } catch (error) {
+      console.error("error al obtener categorias");
     }
+  }, [urlCategoria]);
 
-    filtradoProducto()
+  const filtradoProducto = () => {
+    dataState.productos.forEach((producto) => {
+      if (producto.categorias.idCategoria == id) {
+        //console.log(producto);
+        productosCategoria.push(producto);
+      }
+    });
+  };
 
-    
-    return (
+  filtradoProducto();
+
+  return (
     <div className={styles.categorias}>
-    {categoria?.titulo && <h4 className={styles.tituloFiltroCat}>{categoria.titulo}</h4>}
-    <div
-    className={styles.gridProductos}>
+      {categoria?.titulo && (
+        <h4 className={styles.tituloFiltroCat}>{categoria.titulo}</h4>
+      )}
+      <h4>Productos en esta categoria:</h4>
+      <div className={styles.gridProductos}>
         {productosCategoria.map((producto, index) => (
-                <Card data={producto} key={index}>
-            
-                </Card>
-            ))}
+          <Card data={producto} key={index}></Card>
+        ))}
+      </div>
     </div>
+  );
+};
 
-    </div>
-    )
-}
-
-export default CategoriaFiltro
+export default CategoriaFiltro;
